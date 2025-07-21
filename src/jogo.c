9b65e19ead9d9421f2309e8d43a32cb549e8a9ca
guilherme <guilhermefreires10@gmail.com>
2025-07-21 16:40:19 -0300

    Primeiro commit - criação dos arquivos main.c, jogo.c, editor.c

---
+++ src/jogo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    //Classe com a lógica de negócio do jogo Cladder.
+    
+}
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    //Classe para inicar o jogo.
+}
