399927ee09fd048b871132455ce34cb37b47adc2
guilherme <guilhermefreires10@gmail.com>
2025-07-21 17:01:22 -0300

    Criação da lógica básica de criação e alimentação do arquivo palavras.txt e criação da pasta data

--- src/editor.c
+++ src/editor.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello");
+    FILE *arquivo = fopen("../data/palavras.txt", "a");
+
+    if(arquivo == NULL){
+        printf("Erro ao criar arquivo!\n");
+        return 1;
+    }
+
+    char p1[100], p2[100], p3[100];
+
+    printf("Digite 3 palavras: \n");
+    scanf("%99s %99s %99s", p1, p2, p3);
+
+    fprintf(arquivo, " %s\n %s\n %s\n", p1, p2, p3);
+    fclose(arquivo);
+
+    printf("Palavras salvas no palavras.txt com sucesso!\n");
+    return 0;
 }
